85ed636309bd75e9242e1fd4a7a3a5a271b0976c
mambabusin142@mail.ru <mambabusin142@mail.ru>
2022-11-12 11:45:42 +1000

    Добавьте файлы проекта.

---
+++ Wpf_Yshakow_PR12INS/ApplicationData/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Yshakow_PR12INS.ApplicationData
+{
+    class AppConnect
+    {
+        public static firstEntities modelOdb;
+    }
+}
